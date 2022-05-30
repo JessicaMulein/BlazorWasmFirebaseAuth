6ff934b904f1c732bd51aa8af256f5f04dab1178
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-05-29 21:31:06 -0700

    add login user model

---
+++ Models/Account/LoginUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.Identity.Firebase.Models.Account
+{
+    public class LoginUser
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "The Password field must be a minimum of 6 characters")]
+        public string Password { get; set; }
+    }
+}
